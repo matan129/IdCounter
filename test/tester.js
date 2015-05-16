877d09d4929c2e8e595a5e79f7a22200e4d78d58
Matan Rosenberg <matan129@gmail.com>
2015-05-16 19:35:49 +0300

    Disabled manual tests

--- test/tester.js
+++ test/tester.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 var validator = require('../validator.js');
 
 //"manual" tests
+/*
 describe('\"Deborah\" name tests', function() {
     this.timeout(100000);
     it('Deborah Egli & Deborah Egli',  function (done) {
@@ -29,7 +30,7 @@ describe('\"Deborah\" name tests', function() {
         done();
     });
 });
-
+*/
 
 //random names tests
 describe('15 Random name tests', function() {
