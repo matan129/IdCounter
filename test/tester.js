b2d20ac6d5b085023c3c28f398b98a444080320b
Matan Rosenberg <matan129@gmail.com>
2015-05-16 20:43:20 +0300

    some clenaup

--- test/tester.js
+++ test/tester.js
@@ -2,56 +2,51 @@ var assert = require('assert');
 var validator = require('../validator.js');
 
 //"manual" tests
-describe('\"Deborah\" name tests', function() {
+describe('\"Deborah\" name tests', function () {
     this.timeout(100000);
-    it('Deborah Egli & Deborah Egli',  function (done) {
-        assert.equal(validator.countUniqueNames('Deborah','Egli','Deborah','Egli','Deborah Egli'),1);
-        done();
+    it('Deborah Egli & Debbie Egli', function () {
+        assert.equal(validator.countUniqueNames('Deborah', 'Egli', 'Debbie', 'Egli', 'Debbie Egli'), 1);
     });
 
-    it('Deborah Egli & Debbie Egli',  function (done) {
-        assert.equal(validator.countUniqueNames('Deborah','Egli','Debbie','Egli','Debbie Egli'),1);
-        done();
+    it('Michele Egli & Debbie Egli (two ids)', function () {
+        assert.equal(validator.countUniqueNames('Michele', 'Egli', 'Deborah', 'Egli', 'Michele Egli'), 2);
     });
 
-    it('Deborah Egni & Debbie Egli (Typo)',  function (done) {
-        assert.equal(validator.countUniqueNames('Deborah','Egni','Deborah','Egli','Deborah Egli') ,1);
-        done();
+    it('Deborah Egli & Deborah Egli', function () {
+        assert.equal(validator.countUniqueNames('Deborah', 'Egli', 'Deborah', 'Egli', 'Deborah Egli'), 1);
     });
 
-    it('Deborah S Egli & Debbie Egli (Shorted middle name)',  function (done) {
-        assert.equal(validator.countUniqueNames('Deborah S','Egli','Deborah','Egli','Egli Deborah') ,1);
-        done();
+    it('Deborah Egni & Debbie Egli (Typo)', function () {
+        assert.equal(validator.countUniqueNames('Deborah', 'Egni', 'Deborah', 'Egli', 'Deborah Egli'), 1);
     });
 
-    it('Michele Egli & Debbie Egli (two ids)',  function (done) {
-        assert.equal(validator.countUniqueNames('Michele','Egli','Deborah','Egli','Michele Egli') ,2);
-        done();
+    it('Deborah S Egli & Debbie Egli (Shorted middle name)', function () {
+        assert.equal(validator.countUniqueNames('Deborah S', 'Egli', 'Deborah', 'Egli', 'Egli Deborah'), 1);
     });
 });
 
 
 //random names tests
-describe('15 Random name tests', function() {
+describe('15 Random name tests', function () {
     this.timeout(30000); //TODO improve name lookup.
 
     var input;
 
-    for(var i = 0; i < 5; i++) {
+    for (var i = 0; i < 5; i++) {
         input = generateRandomTest(1);
         it('Random input with 1 identity \n ' + input.desc, function () {
             assert.equal(validator.countUniqueNames(input.bFn, input.bLn, input.sFn, input.sLn, input.bNoC), input.idNumber);
         });
     }
 
-    for(var i = 0; i < 5; i++) {
+    for (var i = 0; i < 5; i++) {
         input = generateRandomTest(2);
         it('Random input with 2 identities \n ' + input.desc, function () {
             assert.equal(validator.countUniqueNames(input.bFn, input.bLn, input.sFn, input.sLn, input.bNoC), input.idNumber);
         });
     }
 
-    for(var i = 0; i < 5; i++) {
+    for (var i = 0; i < 5; i++) {
         input = generateRandomTest(3);
         it('Random input with 3 identities \n ' + input.desc, function () {
             assert.equal(validator.countUniqueNames(input.bFn, input.bLn, input.sFn, input.sLn, input.bNoC), input.idNumber);
@@ -59,7 +54,7 @@ describe('15 Random name tests', function() {
     }
 });
 
-function generateRandomTest(idNumber){
+function generateRandomTest(idNumber) {
     //read data
     var aliasesCSV = 'aliases.csv';
     var lastNamesCSV = 'last_names.csv';
@@ -70,53 +65,53 @@ function generateRandomTest(idNumber){
     var lastNames = fs.readFileSync(lastNamesCSV).toString().split('\r');
 
     //remove control characters (like \r)
-    for(var i = 0; i < famLines.length; i++) {
-        famLines[i] = famLines[i].substr(0,famLines[i].length - 1);
+    for (var i = 0; i < famLines.length; i++) {
+        famLines[i] = famLines[i].substr(0, famLines[i].length - 1);
     }
 
     //build identities.
     var fn1, fn2, fn3, ln1, ln2, ln3;
 
     //choose first name
-    var avaliableFams = famLines[randomize(0,famLines.length,true)].split(',');
-    fn1 = avaliableFams[randomize(0,avaliableFams.length - 1,true)];
+    var avaliableFams = famLines[randomize(0, famLines.length, true)].split(',');
+    fn1 = avaliableFams[randomize(0, avaliableFams.length - 1, true)];
 
-    if(idNumber > 1) {
+    if (idNumber > 1) {
         //generate distinct first name
         do {
             avaliableFams = famLines[randomize(0, famLines.length, true)].split(',');
             fn2 = avaliableFams[randomize(0, avaliableFams.length, true)];
-        } while(areAliases(fn1,fn2,famLines));
+        } while (areAliases(fn1, fn2, famLines));
     } else {
         //generate nickname
-        fn2 = avaliableFams[randomize(0,avaliableFams.length,true)];
+        fn2 = avaliableFams[randomize(0, avaliableFams.length, true)];
     }
 
-    if(idNumber === 3) {
+    if (idNumber === 3) {
         do {
             avaliableFams = famLines[randomize(0, famLines.length, true)].split(',');
             fn3 = avaliableFams[randomize(0, avaliableFams.length, true)];
         } while (areAliases(fn1, fn3, famLines));
     } else {
-        fn3 = avaliableFams[randomize(0,avaliableFams.length,true)];
+        fn3 = avaliableFams[randomize(0, avaliableFams.length, true)];
     }
 
     //choose last name
-    ln1 = lastNames[randomize(0,lastNames.length,true)];
+    ln1 = lastNames[randomize(0, lastNames.length, true)];
 
     //generate distinct last name
-    if(idNumber > 1) {
+    if (idNumber > 1) {
         do {
-            ln2 = lastNames[randomize(0,lastNames.length,true)];
-        } while(ln1 === ln2);
+            ln2 = lastNames[randomize(0, lastNames.length, true)];
+        } while (ln1 === ln2);
     } else { //use the same last name
         ln2 = ln1;
     }
 
-    if(idNumber === 3) {
+    if (idNumber === 3) {
         do {
-            ln3 = lastNames[randomize(0,lastNames.length,true)];
-        } while(ln1 === ln3);
+            ln3 = lastNames[randomize(0, lastNames.length, true)];
+        } while (ln1 === ln3);
     } else {
         ln3 = ln2;
     }
@@ -134,8 +129,8 @@ function generateRandomTest(idNumber){
 
 //determines if name is an alias of the other
 function areAliases(name1, name2, names) {
-    var name1nicks = getAllAliases(name1,names);
-    return name1nicks.indexOf(name2) > - 1
+    var name1nicks = getAllAliases(name1, names);
+    return name1nicks.indexOf(name2) > -1
 }
 
 //generate all aliases of a name
@@ -143,11 +138,11 @@ function getAllAliases(name, nameLines) {
     var allNicknames = [];
 
     //go over the lines we got from the csv
-    for(var i = 0; i < nameLines.length; i++) {
+    for (var i = 0; i < nameLines.length; i++) {
         if (nameLines[i].indexOf(name) > -1) {
             //append them to the allNicknames variable if they match
             var nicks = nameLines[i].split(',');
-            for(var k = 0; k < nicks.length; k++) {
+            for (var k = 0; k < nicks.length; k++) {
                 allNicknames.push(nicks[k]);
             }
         }
@@ -158,7 +153,7 @@ function getAllAliases(name, nameLines) {
 
 //pseudo random number generator
 function randomize(inclusiveMin, exclusiveMax, round) {
-    if(!round)
+    if (!round)
         return Math.random() * (exclusiveMax - inclusiveMin) + inclusiveMin;
     //not normal distribution but close enough
     return (Math.round(Math.random() * (exclusiveMax - inclusiveMin) + inclusiveMin));
